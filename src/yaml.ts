9751d90930974ae16e4bde940f3dd93a5886124c
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-04-01 14:55:20 +0300

    remove unused "fromentries" package

--- src/yaml.ts
+++ src/yaml.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import * as YAML from 'yaml';
-import fromEntries = require('fromentries');
 
 
 // disable "folding" of block scalars, which replaces "|" with ">"
