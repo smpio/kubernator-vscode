2f796a3568146ed07fc058cb46a01ba73c47dbd8
Dmitry Bashkatov <dbashkatov@gmail.com>
2022-04-01 17:54:38 +0300

    remove obsolete code

--- src/kube/openapi.ts
+++ src/kube/openapi.ts
@@ -74,36 +74,3 @@ export async function loadDefinitions(fetch: FetchFunction) {
 
   return scheme.definitions;
 }
-
-// export async function loadDefinitions(fetch: FetchFunction) {
-//   const REF_PREFIX = '#/definitions/';
-//   let scheme = await fetch('openapi/v2');
-//   let namedDefinitions: Definition[] = [];
-
-//   function cleanRecursive(definitions: {[name: string]: Definition}) {
-//     for (let [id, def] of Object.entries(definitions)) {
-//       if ('$ref' in def) {
-//         let ref = def.$ref;
-//         if (!ref.startsWith(REF_PREFIX)) {
-//           throw new Error('Unknown ref: ' + ref);
-//         }
-//         ref = ref.slice(REF_PREFIX.length);
-
-//         definitions[id] = scheme.definitions[ref];
-//       } else if ('properties' in def && def.properties) {
-//         cleanRecursive(def.properties);
-//       } else if (def.type === 'array') {
-//         def.items = cleanRecursive({items: def.items}).items;
-//       }
-
-//       if (def['x-kubernetes-group-version-kind']) {
-//         namedDefinitions.push(def);
-//       }
-//     }
-
-//     return definitions;
-//   }
-
-//   cleanRecursive(scheme.definitions);
-//   return namedDefinitions;
-// }
